b5bf1bf5d940fa7fc04e64ebd9a66afd9f235d76
Karl Parks <kheirlbparks@gmail.com>
2018-08-29 17:17:41 -0700

    Tooks notes and updated comments on file.

--- README.md
+++ README.md
@@ -4,10 +4,43 @@ My personal cloud of class notes and scripts.
 Most present lecture at the top.
 
 ## Lecture 8/29/18
-n/a
+OS loaded on system...
+OS are written in a languages.
+Terminal are a shell on top of the OS.
+I can run my own bash shells using a scripting language.
+
+Unix Commands
+pwd ~ present writing directory (not actually)
+ps ~ processes, shows all processes
+cat ~ copy at terminal (more also does page view)
+
+VI is the editor Kraft recommends.
+Navigation/Editor Modes.
+
+System.out is an object that is an output buffer stream.
+c is not an object orientated language...
+...this means we have a procedural imperative language...
+
+An object orientated language can have classes... they have data and options performed on that data.
+C can't encapsulate anything... very error prone.
+Easier to maintain programs.
+
+Java (Java is Cross Platform):
+.java
+.class
+then .exe
+
+Linux has 3 standard streams:
+standard in
+standard out
+standard error
 
 ## Lecture 8/27/18
 Lost these notes because of Eclipse IDE complication. Very frustrating.
+Here's what I remember:
+
+Make sure you opt out of book.
+Sign up for Rosie is required.
 
 Don't need the book, but probably best to get it.
 Couple different types of programming languages:
--- p1/src/p1.c
+++ p1/src/p1.c
@@ -1,9 +1,20 @@
+/**
+ *  Program 1
+ *  Calculates Optimum Can Size Based on Input of Desired Volumn and Current Material Costs.
+ *  CS320-1
+ *  8/29/18
+ *  @author  Karl Parks cssc0548
+ */
+
 #include <stdio.h>
+#include <math.h>
+//location on karl's computer: cd Documents\GitHub\CS320\p1\src
+//run code using: gcc -lm -Wall -o p1 p1.c
 
 int main(void){
     //Instantiated Varibles
-    double costEnd = 0.00;
-    double costSide = 0.00;
+    double costEndMat = 0.00;
+    double costSideMat = 0.00;
     double vol = 0.00;
     double circum = 0.00;
     double costMin = 0.00;
@@ -20,41 +31,58 @@ int main(void){
     double h = 1.00;
 
     //Input
-    printf("\nProgram #1, csscxxxx, Karl Parks\n");
+    printf("\nProgram #1, cssc0548, Karl Parks\n");
     printf("Enter the cost of end material per square cm:  ");
-    scanf("%lf", &costEnd);
+    scanf("%lf", &costEndMat);
     printf("Enter the cost of the side material per square cm: ");
-    scanf("%lf", &costSide);
+    scanf("%lf", &costSideMat);
     printf("Enter the desired volume in milliliters: ");
     scanf("%lf", &vol);
 
     //Calcs Brute Force
     printf("\n\nBrute Force Answer:\n");
     for (double dia = diaStart; h > 0.001; dia = dia + diaInc) {
+      //assume diamater and calc height
       h = (vol*4.00)/((dia*dia) * pi);
-      //printf("\nHeight: %4.5f",h);
-      costE = ((pi/4.00)*(dia*dia))*costEnd;
+      //calculate the cost of an End
+      costE = ((pi/4.00)*(dia*dia))*costEndMat;
+      //calculate circumference and cost of the rectangular side
       circum = pi*dia;
-      costS = (circum * h)*costSide;
-      //Two Ends!!!!
+      costS = (circum * h)*costSideMat;
+      //Remember, there are TWO Ends to a Can!!!!
       costAdd = costE + costE + costS;
+      //This starts costMin at the right spot so the following if statement does not fail.
       if (costMin == 0.00) {
         costMin = costAdd + 0.01;
       }
+      //Compares costMin to the most recently added cost.
       if (costAdd < costMin) {
         costMin = costAdd;
         dAtMin = dia;
         hAtMin = h;
       }
     }
-
     //Output for Brute
     printf("\nCan Cost: %4.2f",costMin);
     printf("\nDiameter: %4.2f",dAtMin);
     printf("\nHeight: %4.2f",hAtMin);
     printf("\n\n=========================");
+
     //Calcs Calculus
     printf("\n\nCalculus Answer:\n");
+    //Calculus optimized equation found by setting derivate to zero.
+    double dia = cbrt((vol*4.00*costSideMat)/(costEndMat*pi));
+    //Remaining Calcs
+    h = (vol*4.00)/((dia*dia)*pi);
+    costE = ((pi/4.00)*(dia*dia))*costEndMat;
+    circum = pi*dia;
+    costS = (circum * h)*costSideMat;
+    //Two Ends!!!!
+    costAdd = costE + costE + costS;
+    //Output for Calculus
+    printf("\nCan Cost: %4.2f",costAdd);
+    printf("\nDiameter: %4.2f",dia);
+    printf("\nHeight: %4.2f",h);
 
     return 0;
 }
